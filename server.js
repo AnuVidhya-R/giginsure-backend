3a9dac12ce41cf6a6afdd8e2e7a58a1def69523b
Anu Vidhya R <anuashwitha2006@gmail.com>
2026-03-20 11:34:51 +0530

    update CORS config

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const { runDisruptionCheck } = require('./services/disruptionService');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*',
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
